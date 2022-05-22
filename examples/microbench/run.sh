fb1264b6d65d273d22e47a5e9a1a63dd45277eed
Hyesoon Kim <hyesoon@cc.gatech.edu>
2022-05-22 19:52:41 -0400

    succesfully compiled with libvortex

--- examples/microbench/run.sh
+++ examples/microbench/run.sh
@@ -5,7 +5,8 @@ $CuPBoP_PATH/build/compilation/kernelTranslator cudamemcpy_test-cuda-nvptx64-nvi
 $CuPBoP_PATH/build/compilation/hostTranslator cudamemcpy_test-host-x86_64-unknown-linux-gnu.bc host.bc
 llc --relocation-model=pic --filetype=obj  kernel.bc
 llc --relocation-model=pic --filetype=obj  host.bc
-g++ -Wall -L../../build/runtime  -L../../build/runtime/threadPool -o mcpy.out -fPIC -no-pie host.o kernel.o -lc -lx86Runtime -lthreadPool -lpthread
+g++ -Wall -L../../build/runtime  -L../../build/runtime/threadPool  -L${VORTEX_PATH}/driver/stub/ -o mcpy.out -fPIC -no-pie host.o kernel.o  -lc -lx86Runtime    -lvortex  -lthreadPool -lpthread 
+#g++ -Wall -L../../build/runtime  -L../../build/runtime/threadPool  -L -o mcpy.out -fPIC -no-pie host.o kernel.o  -lc -lx86Runtime    -L/usr/local/hyesoon/vortex_public/vortex/driver/stub/libvortex.so  -lthreadPool -lpthread 
 #$ g++ -o mcpy -fPIC -no-pie  -I$CuPBoP_PATH/runtime/threadPool/include  -L$CuPBoP_PATH/build/runtime   -L$CuPBoP_PATH/build/runtime/threadPool  host.o kernel.o  -lpthread -lc -lx86Runtime -lthreadPool
 
 ./mcpy.out -q -v
