4cb221f47767a61cc777e7c810d841c13d337773
Hyesoon Kim <hyesoon@cc.gatech.edu>
2022-05-21 20:29:56 -0400

    fix cudamemcpy to compile with cupbop

--- examples/microbench/cudamemcpy_test.cu
+++ examples/microbench/cudamemcpy_test.cu
@@ -1,18 +1,13 @@
 #include <stdio.h>
 
-__global__
-void saxpy(int n, float a, float *x, float *y)
-{
-  int i = blockIdx.x*blockDim.x + threadIdx.x;
-  if (i < n) y[i] = a*x[i] + y[i];
-}
-
 int main(void)
 {
   int N = 1<<20;
-  float *x, *y, *d_x, *d_y;
+  float *x, *y, *rx, *ry, *d_x, *d_y;
   x = (float*)malloc(N*sizeof(float));
   y = (float*)malloc(N*sizeof(float));
+  rx = (float*)malloc(N*sizeof(float));
+  ry = (float*)malloc(N*sizeof(float));
 
   cudaMalloc(&d_x, N*sizeof(float)); 
   cudaMalloc(&d_y, N*sizeof(float));
@@ -28,15 +23,20 @@ int main(void)
   // Perform SAXPY on 1M elements
   // saxpy<<<(N+255)/256, 256>>>(N, 2.0f, d_x, d_y);
 
-  cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(rx, d_x, N*sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(ry, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
 
-  float maxError = 0.0f;
-  for (int i = 0; i < N; i++)
-    maxError = max(maxError, abs(y[i]-4.0f));
-  printf("Max error: %f\n", maxError);
+  float sumError = 0.0f;
+  for (int i = 0; i < N; i++) {
+   sumError += abs(rx[i]-1.0f);
+   sumError += abs(ry[i]-2.0f);
+  }
+  printf("Sum error: %f\n", sumError);
 
   cudaFree(d_x);
   cudaFree(d_y);
   free(x);
   free(y);
+  free(rx);
+  free(ry); 
 }
