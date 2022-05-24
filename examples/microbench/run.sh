bead992285f0b403936f027e0e5008bc01a0684e
Jaewon Lee <amshoo82@gmail.com>
2022-05-24 12:32:21 -0400

    Generalize the parameters of scripts

--- examples/microbench/run.sh
+++ examples/microbench/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-## cudamemcpy_test
-clang++ -std=c++11  ./cudamemcpy_test.cu -I../.. --cuda-path=$CuPBoP_PATH/cuda-10.1 --cuda-gpu-arch=sm_50 -L$CuPBoP_PATH/cuda-10.1/lib64  -lcudart_static -ldl -lrt -pthread -save-temps -v  || true
-$CuPBoP_PATH/build/compilation/kernelTranslator cudamemcpy_test-cuda-nvptx64-nvidia-cuda-sm_50.bc kernel.bc
-$CuPBoP_PATH/build/compilation/hostTranslator cudamemcpy_test-host-x86_64-unknown-linux-gnu.bc host.bc
+TARGET=cudamemcpy_test
+## TARGET
+clang++ -std=c++11  ./$TARGET.cu -I../.. --cuda-path=$CuPBoP_PATH/cuda-10.1 --cuda-gpu-arch=sm_50 -L$CuPBoP_PATH/cuda-10.1/lib64  -lcudart_static -ldl -lrt -pthread -save-temps -v  || true
+$CuPBoP_PATH/build/compilation/kernelTranslator $TARGET-cuda-nvptx64-nvidia-cuda-sm_50.bc kernel.bc
+$CuPBoP_PATH/build/compilation/hostTranslator $TARGET-host-x86_64-unknown-linux-gnu.bc host.bc
 llc --relocation-model=pic --filetype=obj  kernel.bc
 llc --relocation-model=pic --filetype=obj  host.bc
 g++ -Wall -L../../build/runtime  -L../../build/runtime/threadPool  -L${VORTEX_PATH}/driver/simx/ -o mcpy.out -fPIC -no-pie host.o kernel.o  -lc -lx86Runtime    -lvortex  -lthreadPool -lpthread 
