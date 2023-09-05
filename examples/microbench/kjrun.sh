477dcd2f96860fe157442e3b34508dd33a2dceda
Chihyo Ahn <ahnch@boson.cc.gt.atl.ga.us>
2023-09-05 09:13:28 -0400

    math library file for 64bit was created

--- examples/microbench/kjrun.sh
+++ examples/microbench/kjrun.sh
@@ -6,7 +6,7 @@ set -e
 ######################### Default Varaibles #################################
 DEVICE=vortex
 KERNEL_CU=saxpy.cu
-
+ARCH=64
 #############################################################################
 
 show_usage()
@@ -27,14 +27,20 @@ then
     exit -1
 fi
 
-if [ -z "$RISCV_TOOLCHAIN_PATH" ]
+if [ $ARCH = 32 ]
 then
-    echo "You need to set "\$RISCV_TOOLCHAIN_PATH" environment variable"
+    RISCV_TOOLCHAIN_PREFIX=$RISCV_TOOLCHAIN/riscv32-unknown-elf-
+    RISCV_TOOLCHAIN_FOLDER=$RISCV_TOOLCHAIN
+elif [ $ARCH = 64 ]
+then
+    RISCV_TOOLCHAIN_PREFIX=/opt/riscv64-gnu-toolchain/riscv64-unknown-elf-
+    RISCV_TOOLCHAIN_FOLDER=/opt/riscv64-gnu-toolchain
+else
+    echo "ARCH is setup to a wrong number, check your bash file"
     exit -1
 fi
-RISCV_TOOLCHAIN_PREFIX=$RISCV_TOOLCHAIN/riscv32-unknown-elf-
-CUDA_PATH=$CuPBoP_PATH/cuda-10.1
 
+CUDA_PATH=$CuPBoP_PATH/cuda-10.1
 DEBUG_LEVEL=0
 
 DPRINT()
@@ -105,9 +111,6 @@ $CuPBoP_PATH/build/compilation/hostTranslator $KERNEL-host-x86_64-unknown-linux-
 llvm-dis host.bc 
 llc --relocation-model=pic --filetype=obj host.bc -o host.o
 
-#DPRINT "--- Copy  kernel bitcode for host link"
-#llc --relocation-model=pic --filetype=obj kernel_host.bc
-
 g++ -g -O0 host_vortexrt.cpp -c -o host_vortexrt.o
 
 echo "--- Compile the translated bc code for $DEVICE"
@@ -128,44 +131,41 @@ then
     ${VORTEX_PATH}/sim/simx/simx -r -c 1 -i kernel.bin -s
 elif [ $DEVICE = "vortex" ]
 then
+
     VX_VXFLAGS="-Xclang -target-feature -Xclang +vortex"
     
-    #VX_CFLAGS="-v -O3 -std=c++11 -march=rv32imf -mabi=ilp32f -mcmodel=medany -fno-rtti -fno-exceptions -nostartfiles -fdata-sections -ffunction-sections -I${VORTEX_PATH}/kernel/include -I${VORTEX_PATH}/kernel/../hw"
+    if [ $ARCH = 32 ]
+    then
+        VX_CFLAGS="-v -O3 -std=c++11 -march=rv32imf -mabi=ilp32f -mcmodel=medany -fno-rtti -fno-exceptions -nostartfiles -fdata-sections -ffunction-sections -I${VORTEX_PATH}/kernel/include -I${VORTEX_PATH}/kernel/../hw"
+        VX_LDFLAGS="-Wl,-Bstatic,-T,${VORTEX_PATH}/kernel/linker/vx_link32.ld,--defsym=STARTUP_ADDR=0x80000000 -Wl,--gc-sections ${VORTEX_PATH}/kernel/libvortexrt.a"
+    else
+        VX_CFLAGS="-v -O3 -std=c++11 --sysroot=/opt/riscv64-gnu-toolchain/riscv64-unknown-elf --target=riscv64 -march=rv64imafd -mabi=lp64d -mcmodel=medany -fno-rtti -fno-exceptions -nostartfiles -fdata-sections -ffunction-sections -I${VORTEX_PATH}/kernel/include -I${VORTEX_PATH}/kernel/../hw"
+        VX_LDFLAGS="-Wl,-Bstatic,-T,${VORTEX_PATH}/kernel/linker/vx_link64.ld,--defsym=XLEN=64,--defsym=STARTUP_ADDR=0x180000000 -Wl,--gc-sections ${VORTEX_PATH}/kernel/libvortexrt.a"
+    fi
 
-    #VX_CFLAGS="-v -O3 -std=c++11 --sysroot=/opt/riscv64-gnu-toolchain/riscv64-unknown-elf --target=riscv64 -march=rv64imafd -mabi=lp64d -mcmodel=medany -fno-rtti -fno-exceptions -nostartfiles -fdata-sections -ffunction-sections -I${VORTEX_PATH}/kernel/include -I${VORTEX_PATH}/kernel/../hw"
-    VX_CFLAGS="-v -O3 -std=c++11 --sysroot=/opt/riscv64-gnu-toolchain/riscv64-unknown-elf --target=riscv64 -march=rv64imafd -mabi=lp64d -mcmodel=medany -fno-rtti -fno-exceptions -nostartfiles -fdata-sections -ffunction-sections -I${VORTEX_PATH}/kernel/include -I${VORTEX_PATH}/kernel/../hw"
-
-
-    #VX_LDFLAGS="-Wl,-Bstatic,-T,${VORTEX_PATH}/kernel/linker/vx_link32.ld,--defsym=STARTUP_ADDR=0x80000000 -Wl,--gc-sections ${VORTEX_PATH}/kernel/libvortexrt.a"
-
-    VX_LDFLAGS="-Wl,-Bstatic,-T,${VORTEX_PATH}/kernel/linker/vx_link64.ld,--defsym=XLEN=64,--defsym=STARTUP_ADDR=0x180000000 -Wl,--gc-sections ${VORTEX_PATH}/kernel/libvortexrt.a"
-
-    RISCV_TOOLCHAIN_PATH_64="/opt/riscv64-gnu-toolchain"
-
-    echo "hello1"
+    echo "--- compiling kernel.bc"
     ${LLVM_PREFIX}/bin/clang++ ${VX_CFLAGS} ${VX_VXFLAGS} kernel.bc -c -o kernel.o > kernel.log 2>&1    
-    echo "hello2"
-    ${LLVM_PREFIX}/bin/clang++ ${VX_CFLAGS} --gcc-toolchain=${RISCV_TOOLCHAIN_PATH_64} ../vortex_debug/kernel_wrapper.cpp -c -o kernel_wrapper.o    
+    echo "--- compiling kernel_wrapper.cpp"
+    ${LLVM_PREFIX}/bin/clang++ ${VX_CFLAGS} --gcc-toolchain=${RISCV_TOOLCHAIN_FOLDER} ../vortex_debug/kernel_wrapper.cpp -c -o kernel_wrapper.o    
     ${LLVM_PREFIX}/bin/llvm-objdump -D kernel_wrapper.o > kernel_wrapper.dump
 
-    echo "hello3"
-    ${LLVM_PREFIX}/bin/clang++ ${VX_CFLAGS} --gcc-toolchain=${RISCV_TOOLCHAIN_PATH_64} kernel_wrapper.o kernel.o ${VX_LDFLAGS} -o kernel.elf 
-    #${LLVM_PREFIX}/bin/clang++ ${VX_CFLAGS} --gcc-toolchain=${RISCV_TOOLCHAIN_PATH_64} kernel_wrapper.o kernel.o ${CuPBoP_PATH}/runtime/src/vortex/kernel/cudaKernelImpl.o -lm ${VX_LDFLAGS} -o kernel.elf 
-
-    echo "hello4"
-
-
+    echo "--- compiling kernel.elf"
+    if [ $ARCH = 32 ]
+    then
+        ${LLVM_PREFIX}/bin/clang++ ${VX_CFLAGS} --gcc-toolchain=${RISCV_TOOLCHAIN_FOLDER} kernel_wrapper.o kernel.o ${CuPBoP_PATH}/runtime/src/vortex/kernel/cudaKernelImpl.o -lm ${VX_LDFLAGS} -o kernel.elf 
+    else
+        ${LLVM_PREFIX}/bin/clang++ ${VX_CFLAGS} --gcc-toolchain=${RISCV_TOOLCHAIN_FOLDER} kernel_wrapper.o kernel.o ${CuPBoP_PATH}/runtime/src/vortex/kernel/cudaKernelImpl_64.o -lm ${VX_LDFLAGS} -o kernel.elf 
+    fi
     ${LLVM_PREFIX}/bin/llvm-objcopy -O binary kernel.elf kernel.out    
     ${LLVM_PREFIX}/bin/llvm-objdump -D kernel.elf > kernel.dump
-    #/opt/llvm-vortex/bin/llvm-objdump -mattr=+m,+f,+vortex -D kernel.elf > kernel.dump
 
     echo "--- Kernel compilation completed!"
-    #g++ -g -O0 -Wall -L../../build/runtime  -L../../build/runtime/threadPool  -L${VORTEX_PATH}/driver/stub -I${VORTEX_PATH}/runtime/include -o host.out -fPIC -no-pie host.o host_vortexrt.o  -lc -lvortexRuntime -lvortex -lThreadPool -lpthread 
+    
     g++ -g -O0 -Wall -L../../build/runtime -L../../build/runtime/threadPool -L${VORTEX_PATH}/runtime/stub -I${VORTEX_PATH}/kernel/include -o host.out -fPIC -no-pie host.o host_vortexrt.o  -lc -lvortexRuntime -lvortex -lThreadPool -lpthread 
     echo "--- Host compilation completed!"
-    #simx performance counter settings
+
+    # simx performance counter settings
     export PERF_CLASS=2
-    #LD_LIBRARY_PATH=../../build/runtime/threadPool:${VORTEX_PATH}/driver/simx:../../build/runtime:${LD_LIBRARY_PATH} ./host.out -q -v
     #LD_LIBRARY_PATH=../../build/runtime/threadPool:${VORTEX_PATH}/runtime/simx:../../build/runtime:${LD_LIBRARY_PATH} gdb --arg ./host.out -q -v
     LD_LIBRARY_PATH=../../build/runtime/threadPool:${VORTEX_PATH}/runtime/simx:../../build/runtime:${LD_LIBRARY_PATH} ./host.out -q -v # > host_out.dump
     echo "--- Execution completed!"
